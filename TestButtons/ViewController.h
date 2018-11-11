b7fd4fcfbe005c804dd110431d4bb35be9fb4089
AHFY <anhunfenyu@sina.cn>
2018-11-11 13:31:00 +0800

    commit

---
+++ TestButtons/ViewController.h
@@ -0,0 +1,24 @@
+//
+//  ViewController.h
+//  TestButtons
+//
+//  Created by Jessie on 2018/10/29.
+//  Copyright © 2018年 Jessie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *btn1;
+@property (weak, nonatomic) IBOutlet UIButton *btn2;
+@property (weak, nonatomic) IBOutlet UIButton *btn3;
+@property (weak, nonatomic) IBOutlet UIButton *btn4;
+@property (weak, nonatomic) IBOutlet UIButton *btn5;
+@property (weak, nonatomic) IBOutlet UIButton *btn6;
+@property (weak, nonatomic) IBOutlet UIButton *btn7;
+@property (weak, nonatomic) IBOutlet UIButton *btn8;
+@property (weak, nonatomic) IBOutlet UIButton *btn9;
+
+@end
+
